05154ddc50bbf16e915254eafcfd1d47c819a82c
marcelo dominguez <marceelodominguez@gmail.com>
2023-11-26 16:53:36 -0300

    dot onboardingScreen

---
+++ src/components/onboarding/Dot.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {Dimensions, StyleSheet} from 'react-native';
+import Animated, {
+  Extrapolate,
+  SharedValue,
+  interpolate,
+  interpolateColor,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
+import {COLORS} from '../../util/constants';
+
+type Props = {
+  index: number;
+  x: SharedValue<number>;
+};
+
+const {width: WIDTH_SCREEN} = Dimensions.get('window');
+
+export default function Dot({index, x}: Props) {
+  const animatedDotStyle = useAnimatedStyle(() => {
+    const widthAnimation = interpolate(
+      x.value,
+      [
+        (index - 1) * WIDTH_SCREEN,
+        index * WIDTH_SCREEN,
+        (index + 1) * WIDTH_SCREEN,
+      ],
+      [20, 50, 20],
+      Extrapolate.CLAMP,
+    );
+
+    const opacityAnimation = interpolate(
+      x.value,
+      [
+        (index - 1) * WIDTH_SCREEN,
+        index * WIDTH_SCREEN,
+        (index + 1) * WIDTH_SCREEN,
+      ],
+      [0.5, 1, 0.5],
+      Extrapolate.CLAMP,
+    );
+
+    const backgroundColor = interpolateColor(
+      x.value,
+      [0, WIDTH_SCREEN, 2 * WIDTH_SCREEN],
+      ['#925e1d', '#065238', '#13005A'],
+    );
+
+    return {width: widthAnimation, opacity: opacityAnimation, backgroundColor};
+  });
+
+  return <Animated.View style={[styles.dot, animatedDotStyle]} />;
+}
+
+const styles = StyleSheet.create({
+  dot: {
+    height: 10,
+    borderRadius: 5,
+  },
+});
--- src/screens/OnboardingScreen.tsx
+++ src/screens/OnboardingScreen.tsx
@@ -1,28 +1,47 @@
-import React from 'react';
-import {View, StyleSheet, ViewToken} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, ViewToken, Text, Dimensions} from 'react-native';
 import {onboardingData} from '../data/onboardingData';
 import RenderItem from '../components/onboarding/RenderItem';
-import {Onboarding} from '../interface/onboardingInterface';
 import Animated, {
-  useAnimatedRef,
+  SlideInRight,
+  SlideOutRight,
   useAnimatedScrollHandler,
   useSharedValue,
 } from 'react-native-reanimated';
+import Dot from '../components/onboarding/Dot';
+import {COLORS, FONTS} from '../util/constants';
+
+interface OnViewableItemsChangedInfo {
+  viewableItems: ViewToken[];
+}
+
+const {width} = Dimensions.get('window');
+
+const ITEM_WIDTH = width;
 
 export default function OnboardingScreen() {
-  const flatListRef = useAnimatedRef<Animated.FlatList<Onboarding> | any>();
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const flatListRef = React.useRef(null);
   const x = useSharedValue(0);
   const flatListIndex = useSharedValue(0);
 
   const onViewableItemsChanged = ({
     viewableItems,
-  }: {
-    viewableItems: ViewToken[];
-  }) => {
-    if (viewableItems[0].index !== null) {
-      flatListIndex.value = viewableItems[0].index;
+  }: OnViewableItemsChangedInfo) => {
+    if (viewableItems[0]?.index !== null) {
+      flatListIndex.value = viewableItems[0]?.index;
     }
   };
+
+  const viewabilityConfigCallbackPairs = React.useRef([
+    {
+      viewabilityConfig: {
+        viewAreaCoveragePercentThreshold: 50,
+        minimumViewTime: 600,
+      },
+      onViewableItemsChanged,
+    },
+  ]);
 
   const onScroll = useAnimatedScrollHandler({
     onScroll: event => {
@@ -39,20 +58,61 @@ export default function OnboardingScreen() {
           horizontal
           showsHorizontalScrollIndicator={false}
           pagingEnabled
+          snapToInterval={ITEM_WIDTH}
           scrollEventThrottle={16}
           contentContainerStyle={styles.contentContainerStyle}
           ref={flatListRef}
-          onScroll={onScroll}
-          onViewableItemsChanged={onViewableItemsChanged}
-          viewabilityConfig={{
-            minimumViewTime: 300,
-            viewAreaCoveragePercentThreshold: 10,
+          onMomentumScrollEnd={e => {
+            setCurrentIndex(
+              Math.round(e.nativeEvent.contentOffset.x / ITEM_WIDTH),
+            );
           }}
+          onScroll={onScroll}
+          // onViewableItemsChanged={onViewableItemsChanged}
+          // viewabilityConfig={{
+          //   minimumViewTime: 300,
+          //   viewAreaCoveragePercentThreshold: 10,
+          // }}
+          viewabilityConfigCallbackPairs={
+            viewabilityConfigCallbackPairs.current
+          }
           renderItem={({item, index}) => (
             <RenderItem item={item} index={index} x={x} />
           )}
         />
       </View>
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 65,
+          flexDirection: 'row',
+          alignSelf: 'center',
+          gap: 10,
+        }}>
+        {onboardingData.map((_, index) => (
+          <Dot key={index} index={index} x={x} />
+        ))}
+      </View>
+      {currentIndex === onboardingData.length - 1 && (
+        <Animated.View
+          entering={SlideInRight}
+          exiting={SlideOutRight}
+          style={{
+            position: 'absolute',
+            bottom: 57,
+            right: 22,
+          }}>
+          <Text
+            style={{
+              fontFamily: FONTS.bold,
+              fontSize: 18,
+              color: COLORS.textPrimary,
+              letterSpacing: 0.3,
+            }}>
+            Next
+          </Text>
+        </Animated.View>
+      )}
     </View>
   );
 }
